39b6a629792b5160a1ba6cdc97d32f16877fca84
Moh. Fuad Hamdani <66856511+Fuad-hamdan@users.noreply.github.com>
2020-06-14 13:45:30 +0700

    Add files via upload

---
+++ Perulangan/For.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Counted_Loop;
+
+/**
+ *
+ * @author HP
+ */
+public class For {
+    public static void main(String[] args) {
+        //int i;
+        for (int  i = 0; i < 10 ; i++){
+            System.out.print(i+" ");
+        }
+    }   
+}
+
+        //int variabel
+        //for (nilai awal; kondisi 2; kondisi 3)
---
+++ Perulangan/While.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Counted_Loop;
+
+/**
+ *
+ * @author HP
+ */
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*
+misal :
+        while (kondisi) {
+            kode yg akan dieksekusi
+*/
